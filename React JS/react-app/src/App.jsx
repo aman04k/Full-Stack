050fb590e9f88b8b2d359d44df362415f8434e54
Aman <aman@grow4tech.com>
2024-09-05 18:02:51 +0530

    add

---
+++ React JS/react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return <>
+    <h1>welcome to react</h1>
+      < Navbar />
+      <Card />
+  </>;
+}
+
+export default App;
---
+++ React JS/react-app/src/components/Card.jsx
@@ -0,0 +1,19 @@
+ // import React from 'react'
+
+const Card = () => {
+    return (
+      <div>
+        <div className="card" style={{ width: '18rem' }}>
+          <img src="https://via.placeholder.com/150" className="card-img-top" alt="Card image" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the  content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
+  export default Card;
+  
