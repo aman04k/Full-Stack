88c9070e87e52b16d47111df45465e8eb88ca4c2
Aman <aman@grow4tech.com>
2024-09-08 23:46:42 +0530

    add

---
+++ React JS/BasicReact/src/Card.jsx
@@ -0,0 +1,13 @@
+// import { React } from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta molestias dolor quasi fugiat dicta. Voluptatibus unde quaerat dolorem dignissimos ipsa laborum, voluptatum error officia delectus sapiente quam vero nulla sequi?</p>
+
+        <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae at quia quasi saepe id, odit cumque minima maiores, eveniet, quidem ducimus nobis veritatis rem laboriosam facere labore repellat fuga natus.</h1>
+    </div>
+  )
+}
+
+export default Card
