d17c55f702e6c6c26fe864923f102ae5cf092c3b
Aman <aman@grow4tech.com>
2024-09-09 18:23:41 +0530

    add

---
+++ React JS/Router/src/Blogs.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Blogs = () => {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis modi aspernatur quis consequuntur ullam cumque reprehenderit sed eveniet fugit doloremque beatae cupiditate vero, adipisci nulla tempore similique esse quas minus?</p>
+    </div>
+  )
+}
+
+export default Blogs
---
+++ React JS/Router/src/Home.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>welcome to home page</h1>
+    </div>
+  )
+}
+
+export default Home
